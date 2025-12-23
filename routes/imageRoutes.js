9053abdc181f68d42fd1eda0b0fe6744f1d134d5
huraira <hurairashahidkhanzada@gmail.com>
2025-12-23 16:00:13 +0500

    Add AI

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -71,8 +71,8 @@ router.post("/AiCreation", async (req, res) => {
         model: "Flux1schnell",
         prompt,
         steps: 4,
-        width: 1024,
-        height: 1024,
+        width: 1920,
+        height: 1080,
         seed,
       },
       {
